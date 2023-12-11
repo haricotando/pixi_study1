6cf81519c49a2cfd8e59af2b0178cf7e058966f9
Masaru ANDO <haricotando@gmail.com>
2023-12-11 22:48:49 +0900

    infoボタン、コンテナを分離

--- ApplicationRoot.js
+++ ApplicationRoot.js
@@ -3,6 +3,7 @@ import { GuessContainer } from './guess/GuessContainer.js';
 import { LogContainer } from './log/LogContainer.js';
 import { StartScreen } from './StartScreen.js';
 import AlignHelper from './helper/AlignHelper.js';
+import { InfoContainer } from './InfoContainer.js';
 
 export class ApplicationRoot extends PIXI.Container {
     static keyPadContainer;
@@ -20,7 +21,7 @@ export class ApplicationRoot extends PIXI.Container {
         super();
         // ****************
         this.debug = false;
-        // this.debug = true;
+        this.debug = true;
         // ****************
 
         this.timeoutID = 0;
@@ -57,6 +58,35 @@ export class ApplicationRoot extends PIXI.Container {
 
         this.initBtnInfo();
     }
+
+    initBtnInfo(){
+        const style = new PIXI.TextStyle({
+            fontFamily: 'Material Icons',
+            fontSize: 70,
+            fill: 'gray',
+            });
+
+        this.btnInfo = new PIXI.Text('\ue88e', style);
+        this.addChild(this.btnInfo);
+        this.btnInfo.x = window.innerWidth - this.btnInfo.width -40;
+        this.btnInfo.y = 40;
+        
+        this.btnInfo.on('touchstart', (event) => {
+            this.btnInfo.interactive = false;
+            this.infoContainer = new InfoContainer();
+            this.addChild(this.infoContainer);
+            
+            // const blurFilter = new PIXI.filters.BlurFilter();
+            // blurFilter.blur = 5;
+            // this.filters = [blurFilter];
+            
+        });
+        
+        this.btnInfo.alpha = 0;
+        gsap.timeline().to(this.btnInfo, {alpha:0, duration:0.1}, '+=4').call(()=>{
+            this.btnInfo.interactive = true;
+        }).to(this.btnInfo, {alpha:1, duration:0.3});
+    }
     /* ------------------------------------------------------------
         ゲーム開始のイントロ
     ------------------------------------------------------------ */
@@ -85,10 +115,19 @@ export class ApplicationRoot extends PIXI.Container {
         サブミット
     ------------------------------------------------------------ */
     guessSubmitHandler(){
+
+        if(this.debug){
+            this.guessContainer.reset();
+            this.logContainer.guessMatch('2958');
+            this.guessContainer.matchRainbow();
+            return false;
+        }
+
         this.attempt ++;
         let guessAsText = this.guessList.join('');
         if(guessAsText === this.secretCode){
-            alert('Match');
+            this.guessContainer.reset();
+            this.logContainer.guessMatch(guessAsText);
         }else{
             let isMatch = 0;
             let isIncluded = 0;
@@ -125,73 +164,6 @@ export class ApplicationRoot extends PIXI.Container {
         this.guessList = [];
         this.keyPadContainer.resetKeyPads();
         this.guessContainer.reset();
-    }
-    /* ------------------------------------------------------------
-        Info
-    ------------------------------------------------------------ */
-    initBtnInfo(){
-        // iconStyle
-        const style = new PIXI.TextStyle({
-        fontFamily: 'Material Icons',
-        fontSize: 70,
-        fill: 'gray',
-        });
-        
-        // set container
-        let infoContainer = new PIXI.Container();
-        this.addChild(infoContainer);
-        // infoContainer.pivot.set(infoContainer.width/2, infoContainer.height/2);
-        // infoContainer.x = window.innerWidth / 2;
-        // infoContainer.y = window.innerHeight / 2;
-
-        // background
-        let bg = new PIXI.Graphics();
-        bg.beginFill(0x000000);
-        bg.drawRect(0, 0, window.innerWidth, window.innerHeight);
-        bg.endFill();
-        // bg.pivot.set(bg.width/2, bg.height/2);
-        bg.alpha = 0.1;
-        infoContainer.addChild(bg);
-        infoContainer.visible = false;
-
-        // dialog
-         const dialog = new PIXI.Graphics();
-        dialog.beginFill(0xFFFFFF);
-        dialog.drawRoundedRect(0, 0, 800, 800, 40);
-        dialog.endFill();
-        dialog.pivot.set(400,400);
-        dialog.x = window.innerWidth / 2;
-        dialog.y = window.innerHeight / 2;
-        infoContainer.addChild(dialog);
-
-        const qr = qrcode(0, 'M');
-        qr.addData(window.location.href);
-        qr.make();
-        const qrDataURL = qr.createDataURL(10, 0);
-        const texture = PIXI.Texture.from(qrDataURL);
-        const qrContainer = new PIXI.Sprite(texture);
-        qrContainer.width = 600;
-        qrContainer.height = 600;
-        qrContainer.x = 100;
-        qrContainer.y = 100;
-        dialog.addChild(qrContainer);
-
-        // infoBtn
-        const btnInfo = new PIXI.Text('\ue88e', style);
-        this.addChild(btnInfo);
-        btnInfo.x = window.innerWidth - btnInfo.width -40;
-        btnInfo.y = 40;
-
-        btnInfo.on('touchstart', (event) => {
-            btnInfo.interactive = false;
-            infoContainer.visible = true;
-            bg.interactive = true;
-        });
-        btnInfo.alpha = 0;
-        gsap.timeline().to(btnInfo, {alpha:0, duration:0.1}, '+=4').call(()=>{
-            btnInfo.interactive = true;
-        }).to(btnInfo, {alpha:1, duration:0.3});
-
     }
 
     /* ==================================================
---
+++ InfoContainer.js
@@ -0,0 +1,66 @@
+// import { dataProvider } from '../DataProvider.js';
+
+export class InfoContainer extends PIXI.Container {
+    /* ============================================================
+    Constructor
+    ============================================================ */
+    constructor() {
+        super();
+        this.init();
+    }
+
+    init(){
+        // Background
+        this.bg = new PIXI.Graphics();
+        this.bg.beginFill(0x000000);
+        this.bg.drawRect(0, 0, window.innerWidth, window.innerHeight);
+        this.bg.endFill();
+        this.bg.alpha = 0;
+        this.addChild(this.bg);
+        this.bg.interactive = true;
+        gsap.to(this.bg, {alpha:0.3, duration:0.3});
+
+        // Dialog
+        this.dialog = new PIXI.Graphics();
+        this.dialog.beginFill(0xFFFFFF);
+        this.dialog.drawRoundedRect(0, 0, 800, 800, 40);
+        this.dialog.endFill();
+        this.dialog.pivot.set(400,400);
+        this.dialog.x = window.innerWidth / 2;
+        this.dialog.y = window.innerHeight / 2;
+        this.addChild(this.dialog);
+        this.dialog.scale.x = 1.5;
+        this.dialog.scale.y = 1.5;
+        this.dialog.alpha = 0;
+        gsap.timeline().to(this.dialog.scale, {x:1, y:1, duration:0.3, ease:'expo'})
+        gsap.timeline().to(this.dialog, {alpha:1, duration:0.3, ease:'expo'})
+
+        //
+        this.dialog.interactive = true;
+        this.dialog.on('touchstart', (event) => {
+            gsap.to(this.bg, {alpha:0, duration:0.3});
+            gsap.timeline().to(this.dialog.scale, {x:1.5, y:1.5, duration:0.3, ease:'expo'})
+            gsap.timeline().to(this.dialog, {alpha:0, duration:0.3, ease:'expo'})
+            .call(()=>{
+                this.parent.btnInfo.interactive = true;
+                this.parent.removeChild(this);
+            })
+        });
+
+        // QR
+        const qr = qrcode(0, 'M');
+        qr.addData(window.location.href);
+        qr.make();
+        const qrDataURL = qr.createDataURL(10, 0);
+        const texture = PIXI.Texture.from(qrDataURL);
+        const qrContainer = new PIXI.Sprite(texture);
+
+        this.qrContainer = new PIXI.Sprite(texture);
+        this.qrContainer.width = 600;
+        this.qrContainer.height = 600;
+        this.qrContainer.x = 100;
+        this.qrContainer.y = 100;
+        this.dialog.addChild(this.qrContainer);
+
+    }
+}
